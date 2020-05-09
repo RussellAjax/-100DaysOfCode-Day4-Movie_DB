932af8a15dae0df3f5cf86c01bcbe6ad77678bdd
Richard Russell <russell.ajax@gmail.com>
2020-05-09 12:08:53 +0700

    Add JSON file for data

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
-from flask import Flask
+#Import Flask and jsonify class from
+#the "flask" package.
+from flask import Flask, jsonify
 app = Flask(__name__)
-#Imported the Flask class from flask package
-
-#Define root endpoint with @app.route('/')
-#Remember what an endpoint is: the "main"
-#Address that we are going to visit
-@app.route('/')
-#@app.route() is called a decorator
-#which basically takes function hello()
-#extends its behavior so that it is invoked
-#when / endpoints is requested
 
 
-#hello() returns 
-#{'hello': 'world'}
+#Creating a Movies list that will be converted
+#into a JSON value because of jsonify
+movies = [
+        {
+            "name": "Fight Club",
+            "casts": ["Brad Pitt", "Edward Norton", "Helena Bonham Carter", "Meat Loaf"],
+            "genres": ["Thriller"]
+            },
+        {
+            "name": "The Godfather",
+            "casts": ["Marlon Brando", "Al Pacino", "James caan", "Diane Keaton"],
+            "genres": ["Crime", "Drama"]
+            }
+        ]
 
+#Define root endpoint with @app.route('/movies')
+#This means that the API will be accessible from
+#http://localhost:5000/movies/?false=true
+@app.route('/movies')
+
+#hello() returns the JSON value
+#of the movies list created above
 def hello():
-    return{'hello': 'world'}
+    return jsonify(movies)
 
 #Flask server is started with app.run()
 app.run()
