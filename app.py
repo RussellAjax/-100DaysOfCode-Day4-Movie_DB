39488dd32c4b5ad06042c2d3a7acf50bc20852c0
Richard Russell <9446579+RussellAjax@users.noreply.github.com>
2020-05-14 15:58:59 +0700

    Commit for Day 10; Added exception handlings.

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 #Import Flask, request, and Response class from "flask" package
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from database.db import initialize_db
 from flask_restful import Api
 from resources.routes import initialize_routes
+from flask_jwt_extended import JWTManager
+from resources.errors import errors
 
 app = Flask(__name__)
-api = Api(app)
+app.config.from_envvar('ENV_FILE_LOCATION')
+api = Api(app, errors=errors)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 app.config['MONGODB_SETTINGS'] = {
         'host': 'mongodb://localhost/movie-bag'
--- resources/routes.py
+++ resources/routes.py
@@ -1,4 +1,5 @@
 from .movie import MovieApi, MoviesApi
+from .auth import SignupApi, LoginApi
 
 #Using the routes.py file, you can determine the path of the endpoint
 #Here, we programmed the computer so that the path it follows is /api/movies,
@@ -6,4 +7,5 @@ from .movie import MovieApi, MoviesApi
 def initialize_routes(api):
     api.add_resource(MoviesApi, '/api/movies') #The whole movie document
     api.add_resource(MovieApi, '/api/movies/<id>') #By specific id
-
+    api.add_resource(SignupApi, '/api/auth/signup')
+    api.add_resource(LoginApi, '/api/auth/login')
