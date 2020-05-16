f2834cc13f62855be445a0732deed80c151487d3
Richard Russell <9446579+RussellAjax@users.noreply.github.com>
2020-05-16 08:59:33 +0700

    Add change for Day 11
    Includes file for: forgetting password and creating unit tests.

--- app.py
+++ app.py
@@ -3,19 +3,19 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from database.db import initialize_db
 from flask_restful import Api
-from resources.routes import initialize_routes
 from flask_jwt_extended import JWTManager
 from resources.errors import errors
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_envvar('ENV_FILE_LOCATION')
+mail = Mail(app)
+
+#imports requiring app and mail
+from resources.routes import initialize_routes
 api = Api(app, errors=errors)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
-app.config['MONGODB_SETTINGS'] = {
-        'host': 'mongodb://localhost/movie-bag'
-        }
 initialize_db(app)
 initialize_routes(api)
-app.run()
